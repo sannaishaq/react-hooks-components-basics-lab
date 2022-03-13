cda4952f25a84ecb1a779b851a9dba7278b5abba
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-03-13 15:34:35 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,22 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About(){
+  return (
+    <About>
+    <div id= 'about'>
+    <h2> "About"</h2>
+    </div>
+  </About>
+  )
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home  />
+      <About />
     </div>
   );
 }
